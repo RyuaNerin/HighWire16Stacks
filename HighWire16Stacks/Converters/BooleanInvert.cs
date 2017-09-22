c4930aff41a5a1d4f85cd8aab09e0121f06b33a3
RyuaNerin <admin@ryuanerin.kr>
2017-09-22 22:04:06 +0900

    버그수정

--- HighWire16Stacks/Converters/BooleanInvert.cs
+++ HighWire16Stacks/Converters/BooleanInvert.cs
@@ -8,12 +8,12 @@ namespace HighWire16Stacks.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(value is bool b && b);
+            return value is bool b ? !b : false;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return !(value is bool b && b);
+            return value is bool b ? !b : false;
         }
     }
 }
